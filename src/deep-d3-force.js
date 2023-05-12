3ce79f5066b76767cd15c88e763bf50b9d35b729
Alexandr <l4legenda@gmail.com>
2023-05-12 19:01:05 +0500

    feat: new algorithm

--- src/deep-d3-force.js
+++ src/deep-d3-force.js
@@ -6,76 +6,140 @@ const defaults = require('./defaults');
 const d3 = require('d3-force');
 const assign = require('./assign');
 
+
+let oldCountNodes = 0;
+let simulation = null;
+let activeUUIDTick = '';
+
 class ContinuousLayout {
-  constructor( options ){
-    let o = this.options = assign( {}, defaults, options );
-    this.state = assign( {}, o, {
+
+  constructor(options) {
+    let o = this.options = assign({}, defaults, options);
+    const focusLinkId = options?.deep()?.idLocal('@deep-foundation/core', 'Focus') || 55
+    this.state = assign({}, o, {
       layout: this,
       nodes: o.eles.nodes(),
       edges: o.eles.edges(),
       progress: 0,
       iterations: 0,
-      startTime: 0
-    } );
-    this.simulation = null;
+      startTime: 0,
+      focusLinkId
+    });
     this.removeCytoscapeEvents = null;
     this.destroyedEvent = null;
   }
-  makeBoundingBox ( bb, cy ){
-    if( bb == null ){
+
+  forceLimit(limit) {
+    let nodes;
+
+    function force() {
+      for (const node of nodes) {
+        const speed = Math.hypot(node.vx, node.vy);
+        if (speed > limit) {
+          node.vx = node.vx / speed * limit;
+          node.vy = node.vy / speed * limit;
+        }
+      }
+    }
+
+    force.limit = function (_) {
+      return arguments.length ? (limit = +_, force) : limit;
+    };
+
+    force.initialize = function (_) {
+      nodes = _;
+    };
+
+    return force;
+  }
+
+  makeBoundingBox(bb, cy) {
+    if (bb == null) {
       bb = { x1: 0, y1: 0, w: cy.width(), h: cy.height() };
     } else {
       bb = { x1: bb.x1, x2: bb.x2, y1: bb.y1, y2: bb.y2, w: bb.w, h: bb.h };
     }
-    if( bb.x2 == null ){ bb.x2 = bb.x1 + bb.w; }
-    if( bb.w == null ){ bb.w = bb.x2 - bb.x1; }
-    if( bb.y2 == null ){ bb.y2 = bb.y1 + bb.h; }
-    if( bb.h == null ){ bb.h = bb.y2 - bb.y1; }
-  
+    if (bb.x2 == null) { bb.x2 = bb.x1 + bb.w; }
+    if (bb.w == null) { bb.w = bb.x2 - bb.x1; }
+    if (bb.y2 == null) { bb.y2 = bb.y1 + bb.h; }
+    if (bb.h == null) { bb.h = bb.y2 - bb.y1; }
+
     return bb;
   }
-  setInitialPositionState ( node, state ){
+  hasFocusedLink(node, state) {
+    const focus = node?.data()?.link?.in?.find((link) => {
+      return link.type_id == state?.focusLinkId
+    })
+    const hasFocus = !!focus?.id;
+    return hasFocus;
+  }
+  hasHover(node) {
+    return node?.classes()?.find((classes) => classes == 'hover');
+  }
+  setInitialPositionState(node, state) {
     let p = node.position();
     let bb = state.currentBoundingBox;
-    let scratch = node.scratch( state.name );
-  
-    if( scratch == null ){
+    let scratch = node.scratch(state.name);
+
+    if (scratch == null) {
       scratch = {};
-  
-      node.scratch( state.name, scratch );
+
+      node.scratch(state.name, scratch);
     }
-  
-    assign( scratch, state.randomize ? {
-      x: bb.x1 + Math.round( Math.random() * bb.w ),
-      y: bb.y1 + Math.round( Math.random() * bb.h )
+
+    assign(scratch, state.randomize ? {
+      x: bb.x1 + Math.round(Math.random() * bb.w),
+      y: bb.y1 + Math.round(Math.random() * bb.h)
     } : {
       x: p.x,
-      y: p.y
-    } );
+      y: p.y,
+      old_x: p.x,
+      old_y: p.y,
+      vx: 0,
+      vy: 0,
+    });
     if (node.locked()) {
-      assign( scratch, {
+      assign(scratch, {
         fx: p.x,
         fy: p.y
-      } );
+      });
     }
   }
-  
-  refreshPositions ( nodes, state, fit ){
-    nodes.positions(function( node ){
-      let scratch = node.scratch( state.name );
+
+  refreshPositions(nodes, state) {
+    const hasHover = (node) => this.hasHover(node);
+    const hasFocusedLink = (node, state) => this.hasFocusedLink(node, state);
+    nodes.positions(function (node) {
+      let scratch = node.scratch(state.name);
+      if (hasHover(node)) {
+        const { x, y } = node.position();
+        assign(scratch, {
+          fx: x,
+          fy: y
+        });
+        return { x, y };
+      } else if (!hasHover(node) && !hasFocusedLink(node, state)) {
+        assign(scratch, {
+          fx: undefined,
+          fy: undefined
+        });
+      }
+      assign(scratch, {
+        old_x: scratch.x,
+        old_y: scratch.y,
+      })
       return {
         x: scratch.x,
         y: scratch.y
       };
     });
-    fit && state.cy.fit( state.padding );
   }
 
-  getScratch( el ){
+  getScratch(el) {
     let name = this.state.name;
-    let scratch = el.scratch( name );
+    let scratch = el.scratch(name);
 
-    if( !scratch ){
+    if (!scratch) {
       scratch = {};
 
       el.scratch(name, scratch);
@@ -83,81 +147,93 @@ class ContinuousLayout {
     return scratch;
   }
 
-  ungrabify (nodes) {
-    if( !this.state.ungrabifyWhileSimulating ){ return; }
+  ungrabify(nodes) {
+    if (!this.state.ungrabifyWhileSimulating) { return; }
     nodes.filter(node => {
-      let nodeGrabbable = this.getScratch( node ).grabbable = node.grabbable();
+      let nodeGrabbable = this.getScratch(node).grabbable = node.grabbable();
       return nodeGrabbable;
     });
     nodes.ungrabify();
   }
 
-  regrabify (nodes) {
-    if( !this.state.ungrabifyWhileSimulating ){ return; }
+  regrabify(nodes) {
+    if (!this.state.ungrabifyWhileSimulating) { return; }
     nodes.filter(node => {
-      let nodeGrabbable = this.getScratch( node ).grabbable;
+      let nodeGrabbable = this.getScratch(node).grabbable;
       return nodeGrabbable;
     });
     nodes.grabify();
   }
 
-  tick () {
+  tick(UUIDTick) {
+    if (activeUUIDTick != UUIDTick) {
+      return;
+    }
     const s = this.state;
-    s.progress += 1 / Math.ceil(Math.log(this.simulation.alphaMin()) / Math.log(1 - this.simulation.alphaDecay()));
+    s.progress += 1 / Math.ceil(Math.log(simulation.alphaMin()) / Math.log(1 - simulation.alphaDecay()));
     s.iterations++;
     let _iterations = s.maxIterations && !s.infinite ? s.iterations / s.maxIterations : 0;
     let _timeRunning = Date.now() - s.startTime;
     let _timeIterations = s.maxSimulationTime && !s.infinite ? _timeRunning / s.maxSimulationTime : 0;
     let _progress = Math.max(_iterations, _timeIterations, s.progress);
     _progress = _progress > 1 ? 1 : _progress;
-    if (_progress >= 1) {
-      this.end(!s.infinite);
-      return;
-    }
+
+    let restartAlphaTarget = Math.abs((this.state.alpha || 1) - (this.state.alphaTarget || 0)) / 3;
+    simulation.alphaTarget(restartAlphaTarget).restart();
+
     s.tick && s.tick(_progress);
     if (s.animate) {
-      this.refreshPositions( s.nodes, s, s.fit );
+      this.refreshPositions(s.nodes, s);
     }
   }
 
-  end (destroyed) {
+  end(destroyed) {
     const s = this.state;
-    this.refreshPositions( s.nodes, s, s.fit );
+    this.refreshPositions(s.nodes, s);
     this.emit('layoutstop', s.cy);
     this.reset(destroyed);
+
+    let restartAlphaTarget = Math.abs((this.state.alpha || 1) - (this.state.alphaTarget || 0)) / 3;
+    simulation.alphaTarget(restartAlphaTarget).restart();
   }
 
-  reset(destroyed){
-    this.simulation && this.simulation.stop();
+  reset(destroyed) {
+    // simulation && simulation.stop();
     const s = this.state;
-    this.destroyedEvent && this.destroyedEvent();
+    // this.destroyedEvent && this.destroyedEvent();
     (destroyed || !s.infinite) && this.removeCytoscapeEvents && this.removeCytoscapeEvents();
-    s.animate && this.regrabify( s.nodes );
+    this.regrabify(s.nodes);
     return this;
   }
-  run(){
+  run() {
     this.reset();
     let l = this;
     let s = this.state;
     let ready = false;
-    s.currentBoundingBox = this.makeBoundingBox( s.boundingBox, s.cy );
-    if( s.ready ){ l.one( 'layoutready', s.ready ); }
-    if( s.stop ){ l.one( 'layoutstop', s.stop ); }
-    s.nodes.forEach( n => this.setInitialPositionState( n, s ) );
+    s.currentBoundingBox = this.makeBoundingBox(s.boundingBox, s.cy);
+    if (s.ready) { l.one('layoutready', s.ready); }
+    if (s.stop) { l.one('layoutstop', s.stop); }
+
     if (!ready) {
       ready = true;
       l.emit('layoutready');
     }
-    
-    if (!l.simulation) {
+    const countNodes = s.nodes.length;
+    if (countNodes > 0 && oldCountNodes != countNodes) {
+      oldCountNodes = countNodes;
+      s.nodes.forEach(n => this.setInitialPositionState(n, s));
+      console.log('l.simulation')
+      if (simulation) {
+        simulation.stop();
+      }
       let _forcenodes = s.nodes.map(n => assign(l.getScratch(n), n.data()));
       let _forceedges = s.edges.map(e => assign({}, e.data()));
-      l.simulation = d3.forceSimulation(_forcenodes);
-      s.alpha && l.simulation.alpha(s.alpha);
-      s.alphaMin && l.simulation.alphaMin(s.alphaMin);
-      s.alphaDecay && l.simulation.alphaDecay(s.alphaDecay);
-      s.alphaTarget && l.simulation.alphaTarget(s.alphaTarget);
-      s.velocityDecay && l.simulation.velocityDecay(s.velocityDecay);
+      simulation = d3.forceSimulation(_forcenodes);
+      s.alpha && simulation.alpha(s.alpha);
+      // s.alphaMin && simulation.alphaMin(s.alphaMin);
+      // s.alphaDecay && simulation.alphaDecay(s.alphaDecay);
+      // s.alphaTarget && simulation.alphaTarget(s.alphaTarget);
+      // s.velocityDecay && simulation.velocityDecay(s.velocityDecay);
       let _collide = d3.forceCollide();
       s.collideRadius && _collide.radius(s.collideRadius);
       s.collideStrength && _collide.strength(s.collideStrength);
@@ -186,85 +262,71 @@ class ContinuousLayout {
         s.radialX && _radius.x(s.radialX);
         s.radialY && _radius.y(s.radialY);
       }
-      let _center = d3.forceCenter(s.currentBoundingBox.w / 2, s.currentBoundingBox.h / 2);
-      l.simulation
+      simulation
         .force('collide', _collide)
         .force('link', _link)
-        .force('many-body', _manyBody)
+        .force('charge', _manyBody)
+        .force('limit', this.forceLimit(100))
         .force('x', _x)
-        .force('y', _y)
-        .force("center", _center);
-      _radius && l.simulation.force('radius', _radius);
-      l.simulation
+        .force('y', _y);
+
+      _radius && simulation.force('radius', _radius);
+      const UUIDTick = crypto.randomUUID();
+      activeUUIDTick = UUIDTick;
+      simulation
         .on("tick", () => {
-          l.tick();
+          l.tick(UUIDTick);
         })
         .on("end", () => {
           l.end();
         });
     }
-    if (!l.destroyedEvent) {
-      let destroyHandler;
-      s.cy.one('destroy', destroyHandler = function(){
-        l.stop();
-      });
-      l.destroyedEvent = function () {
-        s.cy.off('destroy', destroyHandler);
-        l.destroyedEvent = null;
-      };
-    }
-    l.prerun( s );
+    l.prerun(s);
     l.emit('layoutstart');
     s.progress = 0;
     s.iterations = 0;
     s.startTime = Date.now();
 
-    if( s.animate ){
-      let restartAlphaTarget = Math.abs((s.alpha || 1) - (s.alphaTarget || 0)) / 3;
-      if (!l.removeCytoscapeEvents) {
-        let _cytoscapeEvent = function(e){
-          let node = this;
-          let pos = node.position();
-          let nodeIsTarget = e.cyTarget === node || e.target === node;
-          if( !nodeIsTarget ){ return; }
-          let _scratch = l.getScratch( node );
-          s.progress = 0;
-          s.iterations = 0;
-          s.startTime = Date.now();
-          _scratch.x = pos.x;
-          _scratch.y = pos.y;
-          if (e.type === 'grab') {
-            l.simulation.alphaTarget(restartAlphaTarget).restart();
-          } else if ((e.type === 'unlock' || e.type === 'free')) {
-            if (!s.fixedAfterDragging) {
-              delete _scratch.fx;
-              delete _scratch.fy;
-            } else {
-              _scratch.fx = pos.x;
-              _scratch.fy = pos.y;
-            }
-            l.simulation.alphaTarget(restartAlphaTarget).restart();
-          } else {
-            _scratch.fx = pos.x;
-            _scratch.fy = pos.y;
-          }
-        };
-        l.removeCytoscapeEvents = function () {
-          s.nodes.off('grab free drag lock unlock', _cytoscapeEvent);
-          l.removeCytoscapeEvents = null;
-        };
-        s.nodes.on('grab free drag lock unlock', _cytoscapeEvent);
-      }
-      l.ungrabify(s.nodes);
+
+    let restartAlphaTarget = Math.abs((s.alpha || 1) - (s.alphaTarget || 0)) / 3;
+    const hasHover = (node) => this.hasHover(node);
+    if (!l.removeCytoscapeEvents) {
+      let _cytoscapeEvent = function (e) {
+        let node = this;
+        let pos = node.position();
+        let nodeIsTarget = e.cyTarget === node || e.target === node;
+        if (!nodeIsTarget) { return; }
+        let _scratch = l.getScratch(node);
+        s.progress = 0;
+        s.iterations = 0;
+        s.startTime = Date.now();
+        _scratch.x = pos.x;
+        _scratch.y = pos.y;
+        console.log("hover", e.type, hasHover(node));
+        if (e.type === 'grab' || hasHover(node)) {
+          simulation.alphaTarget(restartAlphaTarget).restart();
+        } else {
+          _scratch.fx = pos.x;
+          _scratch.fy = pos.y;
+        }
+        console.log("_scratch", _scratch)
+      };
+      l.removeCytoscapeEvents = function () {
+        s.nodes.off('grab free drag lock unlock', _cytoscapeEvent);
+        l.removeCytoscapeEvents = null;
+      };
+      s.nodes.on('grab free drag lock unlock', _cytoscapeEvent);
     }
-    l.postrun( s );
+    l.ungrabify(s.nodes);
+
+    l.postrun(s);
     return this;
   }
 
-  prerun(){}
-  postrun(){}
+  prerun() { }
+  postrun() { }
 
-  stop(){
+  stop() {
     return this.reset(true);
   }
 }
